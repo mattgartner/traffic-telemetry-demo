a71a87fbdb6e95c1f0d79fb2df399fdd24d93368
Matt Gartner <mattgartner@gmail.com>
2018-01-07 19:54:58 -0500

    added test route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../controllers/homeController");
-const {
-  catchErrors
-} = require("../handlers/errorHandlers");
+const {catchErrors} = require("../handlers/errorHandlers");
 
 // ====================================== //
 // ============ Home Routes ============= //
 // ====================================== //
 // for logged out and/or unregistered users
 router.get("/", homeController.homepage);
+router.get("/test", homeController.test);
 
 // for logged in users
 // router.get("/home", authController.isLoggedIn, homeController.homepage);
